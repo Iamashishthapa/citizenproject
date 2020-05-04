36f1e52ae5f6e11046cf29aa3c50cb0f77209808
Ashish Thapa <legendashish66@gmail.com>
2020-05-04 20:54:27 +0545

    SQLALCHemy import probelm

--- mainfolder/config.py
+++ mainfolder/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
